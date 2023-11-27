0d64617c1394277a53811d12abcb50ccccbc195f
szethh <33635766+szethh@users.noreply.github.com>
2023-11-27 21:04:47 +0100

    remove theme switch

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,11 +1,5 @@
-<script>
-	import ThemeSwitch from './theme-switch.svelte';
-</script>
-
 <nav class="flex justify-between p-2">
 	<a href="/"><b>Home</b></a>
-
-	<ThemeSwitch />
 </nav>
 
 <style>
