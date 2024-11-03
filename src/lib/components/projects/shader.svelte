7819e7e30095b8bdd96f12fe5876dc06c3d65d5e
szethh <33635766+szethh@users.noreply.github.com>
2024-11-03 16:36:03 +0100

    skeleton for shaders

---
+++ src/lib/components/projects/shader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	interface Props {
+		shaderId: string;
+		width?: number;
+		height?: number;
+	}
+
+	let { shaderId }: Props = $props();
+</script>
+
+<iframe
+	title="shadertoy"
+	width="640"
+	height="360"
+	frameborder="0"
+	src="https://www.shadertoy.com/embed/{shaderId}?gui=true&t=10&paused=false&muted=false"
+	allowfullscreen
+></iframe>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Shader from '$lib/components/projects/shader.svelte';
+
+	const shaders = ['XcdyDn', 'Mc3cR7'];
+</script>
+
+{#each shaders as shader}
+	<Shader shaderId={shader} />
+{/each}
